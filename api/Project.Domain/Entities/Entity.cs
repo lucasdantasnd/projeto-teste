0fc122f2d3de0763a9937e6d9b31dd5251d30440
lucasdantasnd <lucasdantasnd@gmail.com>
2021-07-19 15:22:15 -0300

    initial commit

---
+++ api/Project.Domain/Entities/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Project.Domain.Entities
+{
+    public abstract class Entity
+    {
+        public Entity(Guid id)
+        {
+            Id = id;
+        }
+
+        public Guid Id { get; private set; }
+
+    }
+}
