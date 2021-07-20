f50aec0b69b2c1d97d8941ef8002792ba1533336
Lucas Dantas <lucasdantasnd@gmail.com>
2021-07-20 14:19:31 -0300

    criando handlers

---
+++ api/Project.Domain/Handlers/Interfaces/IHandler.cs
@@ -0,0 +1,9 @@
+﻿using Project.Domain.Commands.Interfaces;
+
+namespace Project.Domain.Handlers.Interfaces
+{
+    public interface IHandler<T> where T : ICommand
+    {
+        ICommandResult Handle(T command);
+    }
+}
