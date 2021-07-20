73763ea03c023a35703bdc8a03a4defb8d9780fe
Lucas Dantas <lucasdantasnd@gmail.com>
2021-07-20 16:17:24 -0300

    Criando o projet de Infra, adicionando entity frameworkcore e criando o DataContext

--- api/Project.Infra/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Project.Infra
-{
-    public class Class1
-    {
-    }
-}
---
+++ api/Project.Infra/Context/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project.Domain.Entities;
+
+namespace Project.Infra.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("Users");
+            modelBuilder.Entity<User>().Property(x => x.Id);
+            modelBuilder.Entity<User>().Property(x => x.Name).HasMaxLength(120).HasColumnType("varchar(120)");
+            modelBuilder.Entity<User>().Property(x => x.Email).HasMaxLength(160).HasColumnType("varchar(160)");
+        }
+
+    }
+}
